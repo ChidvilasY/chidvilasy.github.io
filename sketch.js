e045a37d05a85c0f01dc0f1f5540fafa16dbdaa2
Chidvilas Yerramsetti <chidduchidvilas@gmail.com>
2019-10-21 20:01:39 +0530

    Added FUGPL License

--- sketch.js
+++ sketch.js
@@ -1,3 +1,34 @@
+// The FUGPL License 
+// ===================
+//
+// Free software should be truly free. The GPL and its derivatives infringe
+// upon the freedoms of the licensee by forcing distribution of code with
+// distribution of software. This license is a protest against anti-business, 
+// anti-freedom licenses such as the GPL.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software with only one restriction: no part of
+// it may be included in software projects that are solely distributed under 
+// strong copyleft restricted licenses such as the GPL, GPL2, or GPL3.
+//
+// There otherwise exists no restrictions in using this software, including
+// without other limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 var a = 255;
 var b = 0;
 var c = 0;
